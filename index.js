b2f9ae7a917d7c94725545ceb98916bd6c301e4d
CarlosMS <programer.cm12@gmail.com>
2026-03-09 09:53:04 -0500

    agregar servicios web para gestionar mascotas (POST, GET, PUT, DELETE)

--- index.js
+++ index.js
@@ -22,6 +22,27 @@ db.connect((err) =>{
 //Iniciar el servidor
 const PORT = 3000
 
+///* SERVICIOS WEB */
+//VERBO = ACCION = INTENCION
+//GET   :Leer
+//POST  :Crear
+//PUT   :Actualizar
+//DELETE:Eliminar
+
+//req (requiere, requerimiento)
+//res (response,respuesta)
+app.post('/mascotas',(req,res)=>{
+    res.send({'proceso': 'POST'})
+})
+app.get('/mascotas',(req,res)=>{
+    res.send({'proceso': 'GET'})
+})
+app.put('/mascotas',(req,res)=>{
+    res.send({'proceso': 'PUT'})
+})
+app.delete('/mascotas',(req,res)=>{
+    res.send({'proceso': 'DELETE'})
+})
 
 app.listen(PORT, ()=>{
     console.log("Servidor iniciado correctamente en http://localhost:3000");

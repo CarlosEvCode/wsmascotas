4955e122c33d005d43bb9e24bc2147aaf5f7041b
CarlosMS <programer.cm12@gmail.com>
2026-03-12 08:56:59 -0500

    Agregar archivo practice2.js con ejemplos de métodos y funciones en JavaScript

---
+++ learn/practice2.js
@@ -0,0 +1,17 @@
+//Que es metodo vs funcion
+//Bloques de instrucciones reutilizables
+//Aceptan parametros y pueden devolver valores
+//Metodo    :POO
+//Funcion   :Programacion estructurada
+
+//Como declarar una funcion?
+//F1: Declaracion standar
+function hacerTarea1() {
+
+}
+
+//Funcion anonima
+/* function (){} */
+
+//Funcion flecha
+// () => {}

2c67f9c3a874321a31880cb1cdf820d6c5d286a7
CarlosMS <programer.cm12@gmail.com>
2026-03-12 08:41:41 -0500

    Agregar archivo practice1.js con ejemplos de variables, arrays y objetos en JavaScript

---
+++ learn/practice1.js
@@ -0,0 +1,26 @@
+// JS lenguaje INTERPRETADO, tipado DINAMICO
+// Variables LEGACY
+var valor = "contenido"
+valor = 15
+valor = true
+
+//Variables
+let nombre = "Jhon"
+nombre = "Edward"
+console.log(nombre);
+
+//Array
+let amigos = ["Pepe", "Lucho"]
+console.log(amigos);
+
+let datosCarrera = {
+    nombre: "Ing de software",
+    ciclo: "V",
+    sede: "Chincha"
+}
+console.log(datosCarrera);
+
+//Constante = VALOR MANTENER
+const igv = 0.18
+const codigo = "A1"
+const lenguajes = ["PHP","Java","Python"]
